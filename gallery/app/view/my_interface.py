762bf0d6fb8e87fadb7e2d65649b9f4de3e3b462
feixiao15 <467534404@qq.com>
2023-05-21 01:13:20 +0800

    有点像秃头人 加了输入框 明天加些按钮（

--- gallery/app/view/my_interface.py
+++ gallery/app/view/my_interface.py
@@ -1,10 +1,9 @@
 # coding:utf-8
 from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtWidgets import QAction, QWidget, QVBoxLayout, QButtonGroup,QLabel
-from qfluentwidgets import LineEdit
 from qfluentwidgets import (FluentIcon, IconWidget, FlowLayout, isDarkTheme,
                             Theme, applyThemeColor, SmoothScrollArea, SearchLineEdit)
-
+from qfluentwidgets import LineEdit
 from .gallery_interface import GalleryInterface
 from ..common.translator import Translator
 from ..common.trie import Trie
@@ -16,7 +15,11 @@ class LineEdit1(SearchLineEdit):
         self.setPlaceholderText(self.tr('Search icons'))
         self.setFixedWidth(305)
         self.textChanged.connect(self.search)
-
+class LineEdit2(LineEdit):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setFixedWidth(305)
+        self.setFixedHeight(100)
 class editPromptInterface(GalleryInterface):
     def __init__(self, parent=None):
         t = Translator()
@@ -26,5 +29,22 @@ class editPromptInterface(GalleryInterface):
             parent=parent
         )
         self.trie = Trie()
+        self.view = QWidget(self)
         self.iconLibraryLabel = QLabel(self.tr('Prompt库'), self)
-        self.searchLineEdit = LineEdit1(self)
+        #self.searchLineEdit = LineEdit1(self)
+        self.input = LineEdit2(self)
+        self.input.setText(self.tr('n m s l！'))
+        self.input.setClearButtonEnabled(True)
+        self.vBoxLayout = QVBoxLayout(self.view)
+        self.__initWidget()
+    def __initWidget(self):
+        self.view.setObjectName('view')
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setWidget(self.view)
+        self.setWidgetResizable(True)
+
+        self.vBoxLayout.setContentsMargins(0, 0, 0, 36)
+        self.vBoxLayout.setSpacing(10)
+        self.vBoxLayout.addWidget(self.iconLibraryLabel)
+        self.vBoxLayout.addWidget(self.input)
+        self.vBoxLayout.setAlignment(Qt.AlignTop)
