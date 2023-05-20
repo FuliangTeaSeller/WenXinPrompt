76b6384c79719aa74e5823644e7ac4c60f8ae74d
feixiao15 <467534404@qq.com>
2023-05-20 13:35:23 +0800

    test push

--- gallery/app/view/my_interface.py
+++ gallery/app/view/my_interface.py
@@ -1,20 +1,20 @@
 # coding:utf-8
 from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtWidgets import QAction, QWidget, QVBoxLayout, QButtonGroup,QLabel
-
+from qfluentwidgets import LineEdit
 from qfluentwidgets import (FluentIcon, IconWidget, FlowLayout, isDarkTheme,
                             Theme, applyThemeColor, SmoothScrollArea, SearchLineEdit)
 
 from .gallery_interface import GalleryInterface
 from ..common.translator import Translator
 from ..common.trie import Trie
-class LineEdit(SearchLineEdit):
+class LineEdit1(SearchLineEdit):
     """ Search line edit """
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setPlaceholderText(self.tr('Search icons'))
-        self.setFixedWidth(304)
+        self.setFixedWidth(305)
         self.textChanged.connect(self.search)
 
 class myInterface(GalleryInterface):
@@ -27,4 +27,4 @@ class myInterface(GalleryInterface):
         )
         self.trie = Trie()
         self.iconLibraryLabel = QLabel(self.tr('Prompt库'), self)
-        self.searchLineEdit = LineEdit(self)
+        self.searchLineEdit = LineEdit1(self)
